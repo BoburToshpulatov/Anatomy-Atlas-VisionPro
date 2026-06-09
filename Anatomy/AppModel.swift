8dc9c809e976f2ad9118892e15bac85e53457771
Toshpulatov Bobur <bobur.vocal@gmail.com>
2026-06-09 05:02:47 +0200

    Cleanup: remove dev/test scaffolding, redundant prepared-assets package, old icon bundle, and dead Learn UI
    - Delete unused DiagnosticImmersiveView / HeartModelTestView / ToggleImmersiveSpaceButton
    - Remove old LearnModeSection + FlowChips + EducationalContent (superseded by LearnCurriculum/LearnReaderView)
    - Delete PreparedLearnAssets (content integrated; images live in Assets.xcassets/Learn)
    - Delete unused AppIcon.icon bundle and References
    - Drop unused learnWindowID

--- Anatomy/AppModel.swift
+++ Anatomy/AppModel.swift
@@ -69,7 +69,6 @@ final class AppModel {
     }
 
     static let immersiveSpaceID = "anatomy-study-space"
-    static let learnWindowID = "anatomy-learn-reader"
 
     let immersiveSpaceID = AppModel.immersiveSpaceID
 
